accc43f5365b68f128a4aedc469c10e8486086bc
Cenker Özkan <68559468+cenkerozkan@users.noreply.github.com>
2024-08-12 23:42:30 +0300

    To refractor the code, Create new composable method for profile image

--- JetBizApp/app/src/main/java/com/example/jetbizapp/MainActivity.kt
+++ JetBizApp/app/src/main/java/com/example/jetbizapp/MainActivity.kt
@@ -6,6 +6,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -19,15 +22,20 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CardElevation
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.jetbizapp.R
 import com.example.jetbizapp.ui.theme.JetBizAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -63,20 +71,38 @@ fun CreateBizCard(){
             .padding(12.dp),
             shape = RoundedCornerShape(corner = CornerSize(15.dp)),
             colors = CardDefaults.cardColors(Color.White),
-            elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)) {
+            elevation = CardDefaults.cardElevation(defaultElevation = 40.dp))
+        {
 
-
-            Surface(modifier = Modifier
-                .size(150.dp)
-                .padding(5.dp),
-                shape = CircleShape,
-                border = BorderStroke(0.5.dp, Color.LightGray),
-                tonalElevation = 4.dp
-            ) {
-
+            Column(modifier = Modifier.height(300.dp).fillMaxWidth(),
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.CenterHorizontally) {
+                CreateImageProfile()
             }
         }
 
+    }
+}
+
+@Composable
+private fun CreateImageProfile() {
+    Surface(
+        modifier = Modifier
+            .size(150.dp)
+            .padding(5.dp),
+        shape = CircleShape,
+        border = BorderStroke(0.5.dp, Color.LightGray),
+        tonalElevation = 4.dp,
+        color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.5f),
+        shadowElevation = 3.dp,
+    )
+    {
+        Image(
+            painter = painterResource(id = R.drawable.profile_image),
+            contentDescription = "profile image",
+            modifier = Modifier.size(135.dp),
+            contentScale = ContentScale.Crop
+        )
     }
 }
 
