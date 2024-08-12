4d269bee8b1c4b32355f821aa345b7fff1a52293
Cenker Özkan <68559468+cenkerozkan@users.noreply.github.com>
2024-08-13 00:47:23 +0300

    Prettify the code, new composable method for portfolio

--- JetBizApp/app/src/main/java/com/example/jetbizapp/MainActivity.kt
+++ JetBizApp/app/src/main/java/com/example/jetbizapp/MainActivity.kt
@@ -1,13 +1,15 @@
-package com.example.myapplication
+package com.example.jetbizapp
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
@@ -22,7 +24,6 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.CardElevation
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -31,7 +32,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -46,41 +46,33 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            JetBizAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) {
-                    CreateBizCard()
-                }
-            }
+            JetBizAppTheme { Scaffold(modifier = Modifier.fillMaxSize()) { CreateBizCard() } }
         }
     }
 }
+
 @Composable
-fun CreateBizCard(){
+fun CreateBizCard() {
     /*
         The Course I am following is using
         material 2, but I use material 3. There
         are some differences between those two,
         so the current usages are like in this code.
     */
-    Surface(modifier = Modifier
-        .fillMaxWidth()
-        .fillMaxHeight()) {
+    Surface(modifier = Modifier.fillMaxWidth().fillMaxHeight()) {
         // The parameter dp is a unit of Android
         // for screen measurements.
-        Card(modifier = Modifier
-            .width(200.dp)
-            .height(390.dp)
-            .padding(12.dp),
+        Card(
+            modifier = Modifier.width(200.dp).height(390.dp).padding(12.dp),
             shape = RoundedCornerShape(corner = CornerSize(15.dp)),
             colors = CardDefaults.cardColors(Color.White),
-            elevation = CardDefaults.cardElevation(defaultElevation = 40.dp))
-        {
-
-            Column(modifier = Modifier
-                .height(300.dp)
-                .fillMaxWidth(),
+            elevation = CardDefaults.cardElevation(defaultElevation = 40.dp)
+        ) {
+            Column(
+                modifier = Modifier.height(300.dp).fillMaxWidth(),
                 verticalArrangement = Arrangement.Top,
-                horizontalAlignment = Alignment.CenterHorizontally) {
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
                 CreateImageProfile()
                 HorizontalDivider(
                     // It was called in Divider in material 2
@@ -94,15 +86,26 @@ fun CreateBizCard(){
                     modifier = Modifier.fillMaxWidth(),
                     color = Color.LightGray,
                     thickness = 1.dp,
-                    )
+                )
                 CreateInfo()
-                Button(onClick = { /*TODO*/ }) {
-                    Text(text = "Portfolio",
-                        style = MaterialTheme.typography.titleMedium)
+                Button(onClick = { Log.d("Clicked", "CreateBizCard: Clicked!!") }) {
+                    Text(text = "Portfolio", style = MaterialTheme.typography.titleMedium)
                 }
+                Content()
             }
         }
+    }
+}
 
+// We use Preview to observe during development.
+// @Preview(showBackground = true)
+@Composable
+fun Content() {
+    Box(modifier = Modifier.fillMaxHeight().fillMaxWidth().padding(5.dp)) {
+        Surface(
+            modifier = Modifier.padding(3.dp).fillMaxWidth().fillMaxHeight(),
+            shape = RoundedCornerShape(corner = CornerSize(6.dp)),
+        ) {}
     }
 }
 
@@ -112,8 +115,7 @@ private fun CreateInfo() {
         modifier = Modifier.padding(5.dp),
         verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.CenterHorizontally
-    )
-    {
+    ) {
         // In typography, instructor uses material 2
         // but there are no instances like subtitle1
         // or h4, So I use the existing ones in material 3
@@ -123,10 +125,7 @@ private fun CreateInfo() {
             color = MaterialTheme.colorScheme.primary
         )
 
-        Text(
-            text = "Android Compose Programmer",
-            modifier = Modifier.padding(3.dp)
-        )
+        Text(text = "Android Compose Programmer", modifier = Modifier.padding(3.dp))
 
         Text(
             text = "@themilesCompose",
@@ -139,16 +138,13 @@ private fun CreateInfo() {
 @Composable
 private fun CreateImageProfile(modifier: Modifier = Modifier) {
     Surface(
-        modifier = Modifier
-            .size(150.dp)
-            .padding(5.dp),
+        modifier = Modifier.size(150.dp).padding(5.dp),
         shape = CircleShape,
         border = BorderStroke(0.5.dp, Color.LightGray),
         tonalElevation = 4.dp,
         color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.5f),
         shadowElevation = 3.dp,
-    )
-    {
+    ) {
         Image(
             painter = painterResource(id = R.drawable.profile_image),
             contentDescription = "profile image",
@@ -160,16 +156,11 @@ private fun CreateImageProfile(modifier: Modifier = Modifier) {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Text(text = "Hello $name!", modifier = modifier)
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    JetBizAppTheme {
-        CreateBizCard()
-    }
+    JetBizAppTheme { CreateBizCard() }
 }
